e7a3d7bc8e5afee224c20a917a507f245e113257
Cikotgr <cikotegar.s@gmail.com>
2022-03-09 22:39:31 +0700

    tes2

---
+++ js/data.js
@@ -0,0 +1,26 @@
+let songs = [
+    {
+        name: 'Puisi Alam',
+        path: '../musics/puisi-alam.mp3',
+        artist: 'Fourtwnty',
+        cover: '../musics/img/puisi alam.png'
+    },
+    {
+        name: 'Kusut',
+        path: '../musics/kusut.mp3',
+        artist: 'Fourtwnty',
+        cover: '../musics/img/kusut.png'
+    },
+    {
+        name: 'Pilu Membiru',
+        path: '../musics/pilu-membiru.mp3',
+        artist: 'Kunto Aji',
+        cover: '../musics/img/pilu membiru.png'
+    },
+    {
+        name: 'Rehat',
+        path: '../musics/rehat.mp3',
+        artist: 'Kunto Aji',
+        cover: '../musics/img/rehat.png'
+    }
+]
